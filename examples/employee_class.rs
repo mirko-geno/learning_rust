3b229ac71d063f264cec16216efb1ada7fc078c4
“mirko-geno” <mirkogenovesi24@gmail.com>
2024-10-04 09:46:00 -0300

    employee_class

---
+++ examples/employee_class.rs
@@ -0,0 +1,25 @@
+struct Employee {
+     name: String,
+     age: u8,
+     salary: f32,
+     balance: f32
+}
+
+impl Employee{
+    fn pay(&mut self){
+        self.balance += self.salary;
+    }
+}
+
+fn main(){
+    let mut employees = Vec::new();
+    employees.push(Employee{name: String::from("Mirko"), age:17, salary:891285.0, balance:0.0});
+    employees.push(Employee{name: String::from("Leo"), age:17, salary:402589.0, balance:0.0});
+    employees.push(Employee{name: String::from("Tiyu"), age:17, salary:28513.0, balance:0.0});
+
+    employees[0].pay();
+
+    for emp in employees{
+        println!("Employee {}, age:{}, salary:{}, balance:{}", emp.name, emp.age, emp.salary, emp.balance);
+    }    
+}
