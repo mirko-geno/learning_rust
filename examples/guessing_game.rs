d4e7510a9641b71a197d684b15ca242732f77e15
“mirko-geno” <mirkogenovesi24@gmail.com>
2024-10-08 10:39:01 -0300

    update

--- examples/guessing_game.rs
+++ examples/guessing_game.rs
@@ -1,14 +1,31 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main(){
     println!("<<<<<Welcome to the guessing game!>>>>>\nEnter a number between 1 and 100 to start!");
-    println!("Please enter your guess");
-
+    let r_number: u8 = rand::thread_rng().gen_range(1..=100);
     let mut guess: String = String::new();
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+    let mut i_guess: u8;
 
-    if guess == "4"{
-        println!("You guessed {}", guess);
+    loop{
+        println!("Please enter your guess");
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+        i_guess = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) =>{
+                println!("Failed, {}", guess);
+                continue}
+        };
+        guess.clear();
+
+        match i_guess.cmp(&r_number){
+            Ordering::Less => println!("Too Small"),
+            Ordering::Greater => println!("Too Big"),
+            Ordering::Equal => {
+                println!("You Won!");
+                break;
+            }
+        }
     }
 }
