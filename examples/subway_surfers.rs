d3d5332b433835bdf51ec868a459aa359777e88e
mirko-geno <mirkogenovesi24@gmail.com>
2024-10-13 20:11:49 -0300

    subway surfers

---
+++ examples/subway_surfers.rs
@@ -0,0 +1,23 @@
+/*
+    Coins (300, 500, 800,  1000) = 69.89%
+    Boards (2-3) = 17%
+    Key (1) = 4%
+    Start booster (1-2) = 2%
+    Score booster (1) = 1%
+    Trophy (1) = 2%
+    Coins (1500 - 2000 - 2500 - 5000) = 4.1%
+    Coins (100000) = 0.01%
+*/
+
+use rand::Rng;
+
+rand.
+
+const MONEY: i32 = 150000;
+const BOX_PRICE: u16 = 300;
+
+
+
+fn  main(){
+    println!("hola")
+}
