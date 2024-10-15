bfe36e0d6dd5243923d2918258ffc9a489c9de9f
“mirko-geno” <mirkogenovesi24@gmail.com>
2024-10-15 09:34:00 -0300

    borrowing code

---
+++ examples/borrowing.rs
@@ -0,0 +1,21 @@
+fn modify_string(string: &mut String){
+    string.push_str("banana");
+}
+
+
+fn main(){
+    // This is horrible
+    let mut stri: String = String::from("El pepe");
+    let mut mut_stri_ref: &mut String = &mut stri;
+    // If we passed stri to println!, mut_stri_ref would be invalid due to the creation of an inmutable reference
+    println!("String is: {mut_stri_ref}");
+    modify_string(mut_stri_ref);
+    println!("String after moficiation is {mut_stri_ref}");
+
+    
+    //Instead it could be done in this way
+    let mut stri: String = String::from("El pepe");
+    println!("String is {stri}"); // Use of inmutable ref
+    modify_string(&mut stri); 
+    println!("String after moficiation is {stri}");
+}
