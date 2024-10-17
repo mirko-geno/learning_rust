d7706fe5cdee7bedcdfdd3584ce3653651a08958
mirko-geno <mirkogenovesi24@gmail.com>
2024-10-16 23:37:25 -0300

    match code example

---
+++ examples/coin_match.rs
@@ -0,0 +1,28 @@
+use rand::Rng;
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dimmel,
+    Quarter
+}
+
+impl Coin {
+    fn in_cents(self) -> u8 {
+        match self {
+            Coin::Penny => {
+                if rand::thread_rng().gen_range(1..=100) <= 5 {println!("Lucky penny");}
+                1
+            },
+            Coin::Nickel => 5,
+            Coin::Dimmel => 10,
+            Coin::Quarter => 25
+        }
+    }
+}
+
+
+fn main() {
+    let coin: Coin = Coin::Penny;
+    println!("{}", coin.in_cents());
+}
