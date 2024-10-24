fa0a55159872999bdfd1b1cd377f1088fe48e403
mirko-geno <mirkogenovesi24@gmail.com>
2024-10-24 00:37:21 -0300

    deleted "moved" folder

--- examples/moved/dangling.rs
+++
@@ -1,9 +0,0 @@
-fn dangle() -> &String {
-    let s = String::from("hello");
-    &s //  returns a reference to data owned by the current function so it's an error
-}
-
-
-fn main() {
-    let _reference_to_nothing = dangle();
-}
