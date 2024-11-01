11fcec2630b356850cd9956c69fb2e2183fce493
“mirko-geno” <mirkogenovesi24@gmail.com>
2024-11-01 09:27:45 -0300

    standard  output stream and error output stream

--- programs/minigrep/src/main.rs
+++ programs/minigrep/src/main.rs
@@ -7,14 +7,14 @@ fn main() {
     // dbg!(&args); // command to verify the content of args
     
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
     println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
+    println!("In file {}:\n", config.file_path);
 
     if let Err(e) = run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1)
     }
 }
