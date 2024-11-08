7d6c00c0f509a2f3b3af4f354fc8698d3d8481c5
mirko-geno <mirkogenovesi24@gmail.com>
2024-11-07 23:58:20 -0300

    Deref, Drop and Rc<T>

--- programs/smart_pointers/src/lib.rs
+++ programs/smart_pointers/src/lib.rs
@@ -1,4 +1,36 @@
+use std::rc::Rc;
+use std::ops::{Deref, Drop};
+
+
+pub struct CustomSmartPointer {
+    pub data: String
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
+
+pub struct MyBox<T>(T);
+
+impl <T> MyBox<T> {
+    pub fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+
 pub enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil
 }
--- programs/smart_pointers/src/main.rs
+++ programs/smart_pointers/src/main.rs
@@ -1,5 +1,42 @@
-use smart_pointers::List::{Cons, Nil};
+use std::rc::Rc;
+
+use smart_pointers::{CustomSmartPointer, List::{Cons, Nil}};
+
+fn hello(name: &str) {
+    println!("Hello {name}!");
+}
 
 fn main() {
-    let cons_list = Cons(1, Cons(2, Cons(3, Nil)));
+    let _cons_list = Cons(1, Rc::new(Cons(2, Rc::new(Cons(3, Rc::new(Nil))))));
+
+    let x: i32 = 5;
+    let r: &i32 = &x;
+    let b: Box<i32> = Box::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *r);
+    assert_eq!(5, *b);
+
+    let boxed_n = Box::new(String::from("Mirko"));
+    hello(&boxed_n);
+
+    let _d = CustomSmartPointer { data: String::from("my stuff") };
+
+    let cd = CustomSmartPointer { data: String::from("some data") };
+    println!("CustomSmartPointer created.");
+    drop(cd);
+    println!("CustomSmartPointer dropped before the end of main.");
+
+
+    //Rc::clone creates a shallow copy and increments the reference counter
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a: {}", Rc::strong_count(&a));
+    let _b = Cons(3, Rc::clone(&a));
+    println!("count after creating b: {}", Rc::strong_count(&a));
+    {
+        let _c = Cons(4, Rc::clone(&a));
+        println!("count after creating c: {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope: {}", Rc::strong_count(&a));
+
 }
