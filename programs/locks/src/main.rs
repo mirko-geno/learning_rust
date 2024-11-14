ef823a15c15533649d19a08502b5cf5caba3b8a8
mirko-geno <mirkogenovesi24@gmail.com>
2024-11-13 23:40:52 -0300

    Mutex<T>

--- programs/locks/src/main.rs
+++ programs/locks/src/main.rs
@@ -1,3 +1,31 @@
+use std::{clone, thread};
+use std::sync::Mutex;
+
+
+fn sum_one(m: Mutex<i32>) {
+    let mut num = m.lock().unwrap();
+    *num += 1;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let counter = Mutex::new(0);
+
+    for _ in 0..10 {
+        thread::spawn(|| sum_one(counter));
+
+    }
+
+
+
+
+
+
+
+    let m = Mutex::new(5);
+
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+    println!("m = {m:?}");
 }
