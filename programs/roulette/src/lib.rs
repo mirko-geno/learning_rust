33649fe4204659e6d347c4b39df9d3924475362b
mirko-geno <mirkogenovesi24@gmail.com>
2024-11-19 20:45:41 -0300

    capped and uncapped bets

--- programs/roulette/src/lib.rs
+++ programs/roulette/src/lib.rs
@@ -14,7 +14,7 @@ impl User {
         self.balance
     }
 
-    pub fn timbear(&mut self, mut bet: f64, cap: i32) {
+    pub fn capped_bet(&mut self, mut bet: f64, cap: i32) {
         let mut rng = rand::thread_rng();
         let mut success = rng.gen_bool(PROB_ROJO);
 
@@ -34,6 +34,34 @@ impl User {
             self.balance += profit;
         } else if _counter > cap {
             println!("Capped!");
+        } else {
+            self.balance -= register;
+            println!("Not enough founds (${}) to pay the next bet (${})", self.balance, bet*2.0);
+            println!("Counter: {_counter}");
+        }
+
+        println!("Probability of event was {}%", PROB_ROJO.powf(_counter.into())*100.0);
+        println!("Highest bet: {bet}");
+    }
+
+    pub fn uncapped_bet(&mut self, mut bet: f64) {
+        let mut rng = rand::thread_rng();
+        let mut success = rng.gen_bool(PROB_ROJO);
+
+        let mut _counter = 1;
+        let profit = bet * 2.0;
+        let mut register = bet;
+
+        while !success && self.balance - register > bet*2.0 {
+            _counter += 1;
+            bet *= 2.0;
+            register += bet;
+            success = rng.gen_bool(PROB_ROJO);
+        }
+
+        if success {
+            println!("Success in {} times!", _counter);
+            self.balance += profit;
         } else {
             self.balance -= register;
             println!("Not enough founds (${}) to pay the next bet (${})", self.balance, bet*2.0);
--- programs/roulette/src/main.rs
+++ programs/roulette/src/main.rs
@@ -2,11 +2,12 @@ use roulette::User;
 
 
 fn main() {
-    let mut user = User::new(1000.0);
+    let mut user = User::new(10000.0);
 
-    let (bet, cap) = (10.0, 4);
-    for _i in 0..100 {
-        user.timbear(bet, cap);
+    let bet = user.get_balance()/100.0;
+    let cap = 4;
+    for _i in 0..10 {
+        user.capped_bet(bet, cap);
     }
     println!("After 10 bets of {bet} the balance is ${}", user.get_balance());
 }
