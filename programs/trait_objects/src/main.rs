7f679cfa702250d655a424b87c4b04ab04277c70
mirko-geno <mirkogenovesi24@gmail.com>
2024-11-25 16:41:17 -0300

    more in depth explanation for trait objects

--- programs/trait_objects/src/main.rs
+++ programs/trait_objects/src/main.rs
@@ -6,9 +6,28 @@ instead of explicitely making two functions for each type, Rust allows creating 
 function that receives a trait object (a parameter that implements certain trait).
 The 'dyn' keyword is a must to achieve this because it indicates that the parameter
 is of an unkwnown size in order to allow recieving non equal object types.
+
+When using generics the compiler makes static dispatch, this means that all
+the implementations made in the generic code will be generated for each nongeneric
+type that received it.
+
+On the other hand, when using trait objects, the dispatch is dynamic, meaning that
+this process occurs on runtime instead of compile time, losing performance. This can
+be fixed by limitating the type a generic parameter can receive to only one that 
+implements certain trait.
+
+The following commented code shows a function that uses a trait object, it isn't 
+efficient so below it there's a code that accomplishes the same functionality 
+without losing efficiency.
 */
 
+/*
 fn print_shape_info(shape: &dyn Shape) {
+    println!("Shape: {}, Area: {}", shape.name(), shape.area());
+}
+*/
+
+fn print_shape_info<T: Shape>(shape:&T) {
     println!("Shape: {}, Area: {}", shape.name(), shape.area());
 }
 
@@ -25,6 +44,7 @@ fn main() {
         Box::new(Rectangle { width: 4.0, height: 5.0 }),
     ];
 
+    println!("Treating shapes as iterator");
     for shape in shapes {
         println!("Shape: {}, Area: {}", shape.name(), shape.area());
     }
