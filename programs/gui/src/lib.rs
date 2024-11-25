46921550381c800ba518374d342a0013992cc446
mirko-geno <mirkogenovesi24@gmail.com>
2024-11-25 16:43:47 -0300

    gui trait object program

--- programs/gui/src/lib.rs
+++ programs/gui/src/lib.rs
@@ -2,6 +2,20 @@ pub trait Draw {
     fn draw(&self) {}
 }
 
+
+pub struct SelectBox {
+    pub width: u32,
+    pub height: u32,
+    pub options: Vec<String>
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+    // code to actually draw a select box
+    }
+}
+
+
 pub struct Image {
     pub width: u32,
     pub height: u32
