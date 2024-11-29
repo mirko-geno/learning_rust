c3873269667da6fc446837f986b8bf9dd861d16b
mirko-geno <mirkogenovesi24@gmail.com>
2024-11-29 18:36:19 -0300

    fixed blog library

--- programs/blog/src/lib.rs
+++ programs/blog/src/lib.rs
@@ -8,26 +8,26 @@ impl Post {
     pub fn new() -> Post {
         Post::Draft(String::new())
     }
-}
 
-impl Post {
     pub fn add_text(&mut self, text: &str) {
         if let Post::Draft(t) = self {
             t.push_str(text);
         }
     }
 
-    pub fn add_text(&mut self, text: &str) {
+    pub fn request_review(&mut self) {
         if let Post::Draft(t) = self {
-            t.push_str(text);
+            *self = Post::WaitingRev(t.to_string())
+        } else {
+            panic!("Post is not Draft variant");
         }
-    } 
-}
+    }
 
-impl Post::WaitingRev {
     pub fn approve(&mut self) {
         if let Post::WaitingRev(t) = self {
             *self = Post::Published(t.to_string());
+        } else {
+            panic!("Post is not WaitingRev variant");
         }
     }
 
