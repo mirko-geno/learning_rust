061c6de4cf650fbc8105371ea6a0c454b4f60d56
mirko-geno <mirkogenovesi24@gmail.com>
2024-11-29 20:55:02 -0300

    functions pattern matching

--- programs/pattern_matching/src/main.rs
+++ programs/pattern_matching/src/main.rs
@@ -1,13 +1,17 @@
-/*
-Pattern matching consists in comparing a value to a pattern, if they match
-a part of code executes allowing the use of that value.
+// Pattern matching consists in comparing a value to a pattern, if they match
+// a part of code executes allowing the use of that value.
 
 
-The following two variants of pattern matching are two ways to do the same,
-while if let can be more readable, match ensures exhaustive checking
-*/
+// Parameters received by a function is pattern matching as well:
+fn print_coordinates(&(x, y): &(i32, i32)) {
+    println!("Current location: ({x}, {y})");
+    }
+
 
 fn main() {
+    // The following two variants of pattern matching are two ways to do the same,
+    // while if let can be more readable, match ensures exhaustive checking
+
     let config_max = Some(3u8);
     match config_max {
         Some(max) => println!("The maximum is configured to be {max}"),
@@ -29,4 +33,7 @@ fn main() {
     for (index, value) in v.iter().enumerate() {
         println!("{value} is at index {index}");
     }
+
+    let point = (3, 5);
+    print_coordinates(&point);
 }
