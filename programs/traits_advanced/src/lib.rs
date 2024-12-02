3fb2041eefa4226b760b19ed2f107dcd67304a47
mirko-geno <mirkogenovesi24@gmail.com>
2024-12-02 16:54:10 -0300

    more advanced trait examples

--- programs/traits_advanced/src/lib.rs
+++ programs/traits_advanced/src/lib.rs
@@ -22,7 +22,6 @@ impl Add for Point {
     }
 }
 
-
 impl Sub for Point {
     type Output = Point;
 
@@ -31,5 +30,33 @@ impl Sub for Point {
             x: self.x - rhs.x,
             y: self.y - rhs.y
         }
+    }
+}
+
+
+pub struct Millimeters(u32);
+impl Millimeters {
+    pub fn new(x: u32) -> Millimeters {
+        Millimeters(x)
+    }
+
+    pub fn value(&self) -> &u32 {
+        &self.0
+    }
+}
+
+pub struct Meters(u32);
+impl Meters {
+    pub fn new(x: u32) -> Meters {
+        Meters(x)
+    }
+}
+
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+    
+    fn add(self, rhs: Meters) -> Self::Output {
+        Millimeters(self.0 + (rhs.0 * 1000))
     }
 }
--- programs/traits_advanced/src/main.rs
+++ programs/traits_advanced/src/main.rs
@@ -1,4 +1,4 @@
-use traits_advanced::Point;
+use traits_advanced::{Point, Millimeters, Meters};
 
 fn main() {
     let p1 = Point::new(2.0, 5.0);
@@ -6,4 +6,13 @@ fn main() {
 
     let p3 = p1 + p2;
     println!("Point 3 x: {}, y: {}", p3.x, p3.y);
+
+
+    let meters = Meters::new(2);
+    let millis = Millimeters::new(250);
+
+    let result_in_millis = millis + meters;
+
+    println!("Result in millis: {}", result_in_millis.value());
+
 }
