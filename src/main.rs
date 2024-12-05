e4011dff06fc5b98d3f99b3b054c01ef3874a74b
mirko-geno <mirkogenovesi24@gmail.com>
2024-12-05 14:41:32 -0300

    finished the book :)

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-use std::{net::TcpListener, thread::{self, JoinHandle}};
-use learning_rust::connection;
+use std::net::TcpListener;
+use learning_rust::{connection, thread_pool::ThreadPool};
 
 fn main() {
     let ip: &str = "127.0.0.1:7878";
     let listener = TcpListener::bind(ip).unwrap();
 
+    let pool = ThreadPool::new(1);
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        thread::spawn( || {
-            connection::handle_connection(stream)
-        });
+        pool.execute(|| connection::handle_connection(stream));
     }
 }
--- src/thread_pool.rs
+++ src/thread_pool.rs
@@ -1,5 +1,4 @@
 use std::{
-    net::TcpStream,
     sync::{Arc, Mutex, mpsc},
     thread::{self, JoinHandle}
 };
@@ -7,9 +6,9 @@ use std::{
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
 
-struct ThreadPool {
+pub struct ThreadPool {
     workers: Vec<Worker>,
-    tx: mpsc::Sender<Job>
+    tx: Option<mpsc::Sender<Job>>
 }
 
 impl ThreadPool {
@@ -25,100 +24,54 @@ impl ThreadPool {
             workers.push(Worker::new(id, Arc::clone(&rx)))
         }
 
-        ThreadPool { workers, tx }
+        ThreadPool { workers, tx: Some(tx) }
     }
 
-    pub fn _execute<F>(&self, f: F)
+    pub fn execute<F>(&self, f: F)
     where
     F: FnOnce() + Send + 'static {
         let job = Box::new(f);
 
-        self.tx.send(job).unwrap();
+        self.tx.as_ref().unwrap().send(job).unwrap();
     }
 }
 
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self.tx.take());
 
-struct Worker {
-    id: usize,
-    thread: JoinHandle<()>,
-    rx: Arc<Mutex<mpsc::Receiver<Job>>>
-}
-
-impl Worker {
-    pub fn new(id: usize, rx: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        Worker {
-            id,
-            thread: thread::spawn(|| {}),
-            rx
-        }
-    }
-}
-
-
-
-/*
-#[allow(dead_code)]
-enum State {
-    Available(JoinHandle<()>),
-    Working(JoinHandle<()>)
-}
-
-impl State {
-    fn join(self) {
-        match self {
-            State::Available(h) | State::Working(h) => {
-                h.join().unwrap()
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
             }
         }
     }
 }
 
 
-pub struct ThreadPool {
-    threads: Vec<State>,
-    queue: Vec<TcpStream>
+struct Worker {
+    id: usize,
+    thread: Option<JoinHandle<()>>,
 }
 
-impl ThreadPool {
-    /// Create a new ThreadPool.
-    ///
-    /// The size is the number of threads in the pool.
-    ///
-    /// # Panics
-    ///
-    /// The `new` function will panic if the size is zero.
-    pub fn new(quant: usize) -> Self {
-        assert!(quant > 0);
-        let mut threads = Vec::new();
+impl Worker {
+    pub fn new(id: usize, rx: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let message = rx.lock().unwrap().recv();
 
-        for _ in 0..quant {
-            threads.push(State::Available(thread::spawn(
-                || println!("Nothing")
-            )));
-        }
-
-        ThreadPool {
-            threads,
-            queue: Vec::new()
-        }
-    }
-
-    pub fn add_to_queue(&mut self, stream: TcpStream) {
-        let length = self.queue.len();
-        self.queue.insert(length,stream);
-    }
-
-    pub fn execute<F>(&self, f: F) 
-    where F: FnOnce() + Send + 'static {
+            match message {
+                Ok(job) => {
+                    println!("Worker {id} got a job; executing.");
+                    job();
+                }
+                Err(_) => {
+                    println!("Worker {id} disconnected; shutting down.");
+                    break;
+                }
+            }
+        });
         
-    } 
-
-    pub fn shutdown(&mut self) {
-        let quant = self.threads.len();
-        for _ in 0..quant {
-            self.threads.pop().unwrap().join();
-        }
+        Worker { id, thread: Some(thread) }
     }
-
 }
-*/
